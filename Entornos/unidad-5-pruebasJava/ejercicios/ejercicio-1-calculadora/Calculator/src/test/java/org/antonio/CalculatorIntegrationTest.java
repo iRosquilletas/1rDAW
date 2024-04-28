88cff0d97dd0c60ec816b15624bf42df4175d936
iRosquilletas <samyelestudio@gmail.com>
2024-04-28 11:29:37 +0200

    Ejercicio Calculator terminado de entornos

--- Entornos/unidad-5-pruebasJava/ejercicios/ejercicio-1-calculadora/Calculator/src/test/java/org/antonio/CalculatorIntegrationTest.java
+++ Entornos/unidad-5-pruebasJava/ejercicios/ejercicio-1-calculadora/Calculator/src/test/java/org/antonio/CalculatorIntegrationTest.java
@@ -1,6 +1,8 @@
 package org.antonio;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,25 +12,33 @@ public class CalculatorIntegrationTest {
     public void testAddAndSubstract() {
         Calculator ej = new Calculator();
         assertEquals(2, ej.add(2, 4), ej.subtract(6, 2));
+        assertTrue(ej.add(2, 3) - ej.subtract(3, 2) == 4);
+        assertFalse(ej.add(2, 3) == ej.subtract(3, 2));
     }
 
     @Test
     public void testAddWithZero() {
         Calculator ej = new Calculator();
-
         assertEquals(5, ej.add(5, 0));
+        assertTrue(ej.subtract(-3, 0) == -3);
+        assertFalse(ej.subtract(10, 0) == 0);
     }
 
     @Test
     public void testSubstractWithZero() {
         Calculator ej = new Calculator();
         assertEquals(10, ej.subtract(10, 0));
+        assertTrue(ej.multiply(2, 3) / ej.divide(6, 3) == 3);
+        assertFalse(ej.multiply(2, 3) == ej.divide(6, 3));
     }
 
     @Test
     public void testMultiplyAndDivide() {
         Calculator ej = new Calculator();
+
         assertEquals(25, ej.multiply(5, ej.divide(10, 2)), 0.0);
+        assertTrue(ej.multiply(2, 3) / ej.divide(6, 3) == 3);
+        assertFalse(ej.multiply(2, 3) == ej.divide(6, 3));
     }
 
     @Test
@@ -36,11 +46,17 @@ public class CalculatorIntegrationTest {
 
         Calculator ej = new Calculator();
         assertEquals(0, ej.multiply(5, 0), 0.0);
+        assertTrue(ej.multiply(5, 0) == 0);
+        assertFalse(ej.multiply(0, 0) == 1);
 
     }
 
     @Test
     public void testDivideWithZero() {
+        Calculator ej = new Calculator();
+
+        assertEquals(0, ej.divide(0, 5), 0.0);
+        // assertTrue da error porque dice que no puede ser infinito.
 
     }
 }
--- Entornos/unidad-5-pruebasJava/ejercicios/ejercicio-1-calculadora/Calculator/src/test/java/org/antonio/CalculatorTest.java
+++ Entornos/unidad-5-pruebasJava/ejercicios/ejercicio-1-calculadora/Calculator/src/test/java/org/antonio/CalculatorTest.java
@@ -9,36 +9,45 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator ej = new Calculator();
-        assertEquals(6, ej.add(4, 2));
-        assertTrue(ej.add(5, 5) >= 10);
+
+        assertEquals(5, ej.add(2, 3));
+        assertTrue(ej.add(-1, 1) == 0);
+        assertFalse(ej.add(2, 3) == 6);
 
     }
 
     @Test
     public void testSubstract() {
         Calculator ej = new Calculator();
-        assertEquals(ej.subtract(5, 5), 5 - 5);
-        assertTrue(ej.add(5, 5) >= 10);
+
+        assertEquals(1, ej.subtract(3, 2));
+        assertTrue(ej.subtract(5, 5) == 0);
+        assertFalse(ej.subtract(10, 3) == 9);
     }
 
     @Test
     public void testMultiply() {
         Calculator ej = new Calculator();
-        assertEquals(16, ej.multiply(4, 4), 0.0);
-        assertTrue(ej.multiply(4, 4) >= 10);
+
+        assertEquals(6, ej.multiply(2, 3), 0.0);
+        assertTrue(ej.multiply(5, 0) == 0);
+        assertFalse(ej.multiply(2, 3) == 10);
 
     }
 
     @Test
     public void testDivide() {
         Calculator ej = new Calculator();
-        assertEquals(ej.divide(10, 2), 5, 0.0);
+
+        assertEquals(2.5f, ej.divide(5, 2), 0.001);
+        assertTrue(ej.divide(10, 5) == 2);
+        assertFalse(ej.divide(8, 2) == 3);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testDivideByZero() {
         Calculator ej = new Calculator();
-        assertEquals(0, ej.divide(0, 2), 0.0);
+        assertEquals(0, ej.divide(2, 0), 0.0);
     }
 
 }
